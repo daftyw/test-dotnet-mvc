aa2235e01fb92f97b6823b38d2ec069186b3eb41
Rawin Ngamloet <Rawin.n@augmentis.biz>
2017-06-07 17:29:19 +0700

    * demo-aspnet-mvc.sln: * Web.config: * Global.asax: * Global.asax.cs: * packages.config: * app.css: * Web.config: * bootstrap.js: * bootstrap.css: * demo-aspnet-mvc.csproj: * jquery-3.1.1.js: * _ViewStart.cshtml: * Index.cshtml: * bootstrap.min.js: * RouteConfig.cs: * Index1.cshtml: * BundleConfig.cs: * bootstrap.min.css: * bootstrap.css.map: * Error.cshtml: * modernizr-2.8.3.js: * _Layout.cshtml: * jquery-3.1.1.min.js: * bootstrap-theme.css: * jquery-3.1.1.slim.js: * jquery-3.1.1.min.map: * jquery-3.1.1-vsdoc.js: * bootstrap.min.css.map: * HomeController.cs: * UserController.cs: * bootstrap-theme.min.css: * bootstrap-theme.css.map: * jquery-3.1.1.slim.min.js: * jquery-3.1.1.slim.min.map: * _LoginPartial.cshtml: * bootstrap-theme.min.css.map: * glyphicons-halflings-regular.ttf: * glyphicons-halflings-regular.eot: * glyphicons-halflings-regular.svg: * glyphicons-halflings-regular.woff: * glyphicons-halflings-regular.woff2:
    * .gitignore: Init done

---
+++ demo-aspnet-mvc/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace demoaspnetmvc
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-3.1.1.min.js"));
+
+			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+				"~/Scripts/modernizr-2.8.3.js"));
+            
+			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/app.css",
+                "~/Content/bootstrap.css",
+                "~/Content/bootstrap-theme.css"));
+        }
+    }
+}
---
+++ demo-aspnet-mvc/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demoaspnetmvc.Controllers
+{
+    public class UserController : Controller
+    {
+        public string Index() {
+            return "Hello, <h3>Worlds</h3>";
+        }
+
+        public ActionResult Index1() {
+            return View();
+        }
+
+        public string Welcome(string name, int id = 1) {
+            return HttpUtility.HtmlEncode("Welcome to my world, " + name + ", ID = " + id);
+        }
+    }
+}
